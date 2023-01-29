bbab65116a432a9006df7cc614464c7788d4955d
TakenokoSato <kosuke060531@yahoo.co.jp>
2023-01-29 18:28:47 +0900

    再提出「税率追加」

--- src/Review01.java
+++ src/Review01.java
@@ -3,14 +3,15 @@ public class Review01 {
     public static void main(String[] args) {
         // int型の引数を持つtaxメゾットを呼び出す。
         int price = 1500;
-        int taxOfPrice = tax(price);
+        int taxRate = 10;
+        int taxOfPrice = tax(price,taxRate);
         int TaxIncludedPrice = price+taxOfPrice;
 
        System.out.println(price+"円の商品の税込価格は"+TaxIncludedPrice+"円（消費税は"+taxOfPrice+"円）です。");
     }
 
-    public static int tax(int num1) {
-        int taxOfPrice =num1/10;
+    public static int tax(int price,int taxRate) {
+        int taxOfPrice =price/taxRate;
         return taxOfPrice;
     }
 
