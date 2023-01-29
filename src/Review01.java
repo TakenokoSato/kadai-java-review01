11f51fd2418c6ba7c08b1d592c6d8a0ddf693256
TakenokoSato <kosuke060531@yahoo.co.jp>
2023-01-29 18:44:50 +0900

    再々提出

--- src/Review01.java
+++ src/Review01.java
@@ -2,16 +2,16 @@ public class Review01 {
 
     public static void main(String[] args) {
         // int型の引数を持つtaxメゾットを呼び出す。
-        int price = 1500;
-        int taxRate = 10;
+        int price = 2000;
+        double taxRate = 10.0;
         int taxOfPrice = tax(price,taxRate);
         int TaxIncludedPrice = price+taxOfPrice;
 
        System.out.println(price+"円の商品の税込価格は"+TaxIncludedPrice+"円（消費税は"+taxOfPrice+"円）です。");
     }
 
-    public static int tax(int price,int taxRate) {
-        int taxOfPrice =price/taxRate;
+    public static int tax(int price,double taxRate) {
+        int taxOfPrice =(int)(price/100*taxRate);
         return taxOfPrice;
     }
 
